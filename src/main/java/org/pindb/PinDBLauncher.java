9c224ea6de431f7cf1648ca19775ed46e5ebd551
Kevin <mccreeper1319@icloud.com>
2026-07-27 12:49:33 -0400

    Fix packaged JavaFX launcher

---
+++ src/main/java/org/pindb/PinDBLauncher.java
@@ -0,0 +1,12 @@
+package org.pindb;
+
+import javafx.application.Application;
+
+public final class PinDBLauncher {
+    private PinDBLauncher() {
+    }
+
+    public static void main(String[] args) {
+        Application.launch(PinDBApplication.class, args);
+    }
+}
