4e7b7b8f2329b036bb3a81400b1f1e2b7703be6b
Kevin <mccreeper1319@icloud.com>
2026-08-16 17:27:02 -0400

    Make release packages platform neutral

--- src/main/java/org/pindb/service/ReleasePackage.java
+++ src/main/java/org/pindb/service/ReleasePackage.java
@@ -1,13 +1,13 @@
 package org.pindb.service;
 
-import org.pindb.platform.LinuxPackageType;
+import org.pindb.platform.PackageType;
 import org.pindb.platform.SystemArchitecture;
 
 import java.net.URI;
 import java.util.Objects;
 
 public record ReleasePackage(
-        LinuxPackageType type,
+        PackageType type,
         SystemArchitecture architecture,
         String fileName,
         URI downloadUri,
